deb073d7edf23dcd54f7b1d81c094a270d41a6ab
sibi-narendran <sibinarendran@gmail.com>
2025-11-02 21:32:42 +0530

    cloudflare 1

--- worker-configuration.d.ts
+++ worker-configuration.d.ts
@@ -24,4 +24,6 @@ interface Env {
   SUPABASE_DEFAULT_PROJECT_ID: string;
   VITE_SUPABASE_URL: string;
   VITE_SUPABASE_ANON_KEY: string;
+  SUPABASE_SERVICE_ROLE_KEY: string;
+  DATABASE_URL: string;
 }
